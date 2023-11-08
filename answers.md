64ddf9b55e5fbe27bf5a2e992f4ee90adada71e6
Alex040711 <zli44@tulane.edu>
2023-11-07 22:53:22 -0600

    Alex1107

--- answers.md
+++ answers.md
@@ -1,20 +1,14 @@
 # CMPS 2200 Recitation 06
+
 ## Answers
 
+**Name:**_______Alex Li__________________
 **Name:**_________________________
-**Name:**_________________________
-
 
 Place all written answers from `recitation-07.md` here for easier grading.
 
-
-
-- **2)**
-
-- **3)**
-
-- **4)**
-
-- **6)**
-
-- **8)**
+- **2)   W(**n**)**=W(**n**−**1**)**+W(**n**−**2**)**+c  for **n**>**1,    work: 2^n**
+- **3)  **S**(**n**)**=**S**(**n**−**1**)**+**d**,  work: 2^n**
+- **4) This pattern is similar to the Fibonacci sequence itself. you would see an exponential increase, and each value except the first two would be higher than the corresponding Fibonacci number**
+- **6) W(n)=n, S(n)=n**
+- **8) 2, W(n)=n, S(n)=1**
--- main.py
+++ main.py
@@ -5,19 +5,49 @@ def fib_recursive(n, counts):
     each time fib_recursive(i, counts) is called.
     """    
     counts[n] += 1
-    ###TODO
-    pass
+    # Base cases: F0 = 0 and F1 = 1
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        # Recursive case: Fn = Fn-1 + Fn-2
+        return fib_recursive(n-1, counts) + fib_recursive(n-2, counts)
     
 
     
 def fib_top_down(n, fibs):
-    ###TODO
-    pass
+    # Base case: if fibs[n] has been computed, return it
+    if fibs[n] != -1:
+        return fibs[n]
+    # Base cases: F0 = 0 and F1 = 1
+    if n == 0:
+        fibs[n] = 0
+        return 0
+    elif n == 1:
+        fibs[n] = 1
+        return 1
+    else:
+        # Recursive case: compute Fn, store it in fibs[n], and return it
+        fibs[n] = fib_top_down(n-1, fibs) + fib_top_down(n-2, fibs)
+        return fibs[n]
 
 
 def fib_bottom_up(n):
-    ###TODO
-    pass
+    # If n is 0 or 1, return n (since F0 = 0, F1 = 1)
+    if n <= 1:
+        return n
+
+    # Initialize a list of n+1 elements to store the Fibonacci sequence
+    fibs = [0] * (n + 1)
+    fibs[1] = 1  # F1 = 1
+
+    # Iterate from 2 to n, filling in the Fibonacci sequence in the list
+    for i in range(2, n + 1):
+        fibs[i] = fibs[i - 1] + fibs[i - 2]
+
+    # Return the nth Fibonacci number
+    return fibs[n]
 
 
 
